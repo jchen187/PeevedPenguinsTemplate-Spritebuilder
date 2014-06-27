a60f348c53970f30d2459f56f2e6191563c0a298
Johnny Chen <Johnny@Johnnys-MacBook-Pro.local>
2014-06-27 09:45:48 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,36 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Johnny Chen on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay{
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+//is called when CCB file has completed loading
+- (void)didLoadFromCCB{
+    //tell this scene to accept touches
+    self.userInteractionEnabled = TRUE;
+}
+
+//called on every touch in this scene
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    [self launchPenguin];
+}
+
+- (void)launchPenguin{
+    //loads the penguin.ccb we have set up in SpriteBuilder
+    CCNode *penguin = [CCBReader load:@"Penguin"];
+    //position the penguin at the bowl of the catapult
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
+    
+    //add the penguin to the physicsNode of this scene(because it has physics enabled)
+    
+}
+@end
