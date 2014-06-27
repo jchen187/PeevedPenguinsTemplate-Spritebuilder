01549d78525dd80d3a494dd8e697289599402061
Johnny Chen <Johnny@johnnys-mbp.home>
2014-06-26 22:47:27 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,22 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Johnny Chen on 6/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init{
+    self = [super init];
+    //the instance of seal that is created by calling the init of the super calss
+    //so we are actually overriding the init of the super class
+    if  (self) {
+        CCLOG(@"Seal created");
+    }
+    return self;
+}
+@end
